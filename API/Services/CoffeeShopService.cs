09dd7f0c72d23d5a020ae80ca48b52c902efa062
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-11-19 06:01:20 +0300

    A VERY USEFULL MAPPING LIKE SQL QUERY BUT ITS NOT DO IT SOMEWHERE THAT IS ON COFFEESHOPSERVICE

---
+++ API/Services/CoffeeShopService.cs
@@ -0,0 +1,27 @@
+﻿using API.Models;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class CoffeeShopService : ICoffeeShopService
+    {
+        private readonly ApplicationDbContext context;
+
+        public CoffeeShopService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<CoffeeShopModel>> GetAll()
+        {
+            var coffeeShops = await (from shop in context.CoffeeShops select new CoffeeShopModel()
+            {
+                Id = shop.Id,
+                Name = shop.Name,
+                Address = shop.Address,
+                OpeningHours = shop.OpeningHours
+            }).ToListAsync();
+        }
+    }
+}
