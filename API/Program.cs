23da730752dcefa8dc627ba27a6fb2222f0866ff
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-11-21 11:22:28 +0300

    API updated

--- API/Program.cs
+++ API/Program.cs
@@ -3,22 +3,15 @@ using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConn"));
 });
 builder.Services.AddScoped<ICoffeeShopService, CoffeeShopService>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
